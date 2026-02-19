b9d93f84b809d2317ff997834ea18d94c3a092aa
mancinilucas <mancini.lucasg@gmail.com>
2026-02-19 20:22:15 -0300

    first commit

---
+++ src/main/java/com/app/vertex/VertexApplication.java
@@ -0,0 +1,13 @@
+package com.app.vertex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VertexApplication.class, args);
+	}
+
+}
